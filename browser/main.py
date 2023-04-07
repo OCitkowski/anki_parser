e2c26f89667607e3043098fa57ac28f059105034
fox <o.citkowski@gmail.com>
2023-04-07 11:07:53 +0200

    +

--- browser/main.py
+++ browser/main.py
@@ -1,29 +1,43 @@
 import multiprocessing
+import random
+
 from browser.chrome_browser.chrome_browser import DriverChrome
 import requests
 
 from browser.chrome_browser.options import REQUEST_TIMEOUT
 
 
-class Browser(DriverChrome):
+class TranslateBot(DriverChrome):
     def __init__(self, proxy: str = None):
         super().__init__(proxy=proxy)
 
 
-def get_proxis_from_txt(filename):
+def get_proxies_from_txt(file_name: str) -> list:
     proxies = []
     try:
-
-        with open(filename, 'r') as f:
+        with open(file_name, 'r') as f:
             lines = f.readlines()
-
             for line in lines:
                 proxies.append(line)
-    except Exception as e:
-        pass
-    finally:
-        f.close()
+    except FileNotFoundError:
+        print(f"Error: File '{file_name}' not found.")
     return proxies
+
+
+def delete_bad_proxies_from_txt(file_name: str) -> None:
+    try:
+        with open(file_name, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if check_proxy(line):
+                    pass
+                else:
+                    lines.remove(line)
+            with open(file_name, 'w') as f:
+                f.write("".join(lines))
+    except FileNotFoundError:
+        print(f"Error: File '{file_name}' not found.")
+
 
 def check_proxy(proxy):
     try:
@@ -36,8 +50,9 @@ def check_proxy(proxy):
     except:
         return False
 
-def main(times_sleep, proxy):
-    driver = Browser(proxy=proxy)
+
+def main(times_sleep, proxy, link):
+    driver = TranslateBot(proxy=proxy)
     driver.get('https://www.google.com/')
     driver.sleep(5)
     driver.cookies_file_name = 'hello'
@@ -50,23 +65,20 @@ def wrapper(args):
 
 
 if __name__ == '__main__':
-
-    # start = []
-    # for i in range(5):
-    #     start.append({'times_sleep': 10, 'open_browser': True})
+    # processes = []
+    # # for i in range(10):
+    # for proxy in random.choice(get_proxies_from_txt('free_proxy_test.txt')):
+    #     start_args = ({'times_sleep': 20, 'proxy': proxy})
+    #     p = multiprocessing.Process(target=wrapper, args=(start_args,))
+    #     p.start()
+    #     processes.append(p)
+    #     print(proxy)
     #
-    # with multiprocessing.Pool(processes=5) as pool:
-    #     pool.map(wrapper, start)
+    # for p in processes:
+    #     p.join()
 
-    processes = []
-    # for i in range(10):
-    for proxy in get_proxis_from_txt('free_proxy_test.txt'):
-        if check_proxy(proxy):
-            start_args = ({'times_sleep': 20, 'proxy': proxy})
-            p = multiprocessing.Process(target=wrapper, args=(start_args,))
-            p.start()
-            processes.append(p)
-            print(proxy)
-
-    for p in processes:
-        p.join()
+    filename = 'free_proxy_test.txt'
+    delete_bad_proxies_from_txt(filename)
+    pr = get_proxies_from_txt(filename)
+    print(pr)
+    print(random.choice(pr))
